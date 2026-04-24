ea69aa95b32f8513008eb04458e8d89884f2a0a1
floraSimpleDev <samantha20200604@gmail.com>
2026-04-24 17:20:54 +1000

    feat: register routs

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,26 @@
 import Fastify, { type FastifyInstance } from 'fastify'
 
+import authenticate from '@/plugins/authenticate'
+import authZero from '@/plugins/authZero'
+import cors from '@/plugins/cors'
+import database from '@/plugins/database'
+import swagger from '@/plugins/swagger'
+import adminRoute from '@/routes/adminRoute'
 import healthRoute from '@/routes/healthRoute'
-
-import swagger from './plugins/swagger'
+import meRoute from '@/routes/meRoute'
 
 const buildApp = async (): Promise<FastifyInstance> => {
   const app = Fastify({ logger: true })
 
+  await app.register(cors)
   await app.register(swagger)
+  await app.register(database)
+  await app.register(authZero)
+  await app.register(authenticate)
 
   await app.register(healthRoute)
+  await app.register(meRoute)
+  await app.register(adminRoute)
 
   return app
 }
